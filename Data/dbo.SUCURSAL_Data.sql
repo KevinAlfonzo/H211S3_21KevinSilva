5fbd28dc2e473f25ea1ad4ef8b7ff66d2a4b827d
Kevin Silva Alfonzo <kevin.silva@vallegrande.edu.pe>
2022-07-08 13:11:45 -0500

    Subiendo Datos

---
+++ Data/dbo.SUCURSAL_Data.sql
@@ -0,0 +1,12 @@
+SET IDENTITY_INSERT [dbo].[SUCURSAL] ON
+INSERT INTO [dbo].[SUCURSAL] ([IDSUC], [NOMSUC], [DIRSUC], [IDJEFSUC]) VALUES (1, 'PizzaHut San Vicente', 'Av. Los Portales', 1)
+INSERT INTO [dbo].[SUCURSAL] ([IDSUC], [NOMSUC], [DIRSUC], [IDJEFSUC]) VALUES (2, 'PizzaHut Imperial', 'Jr. Benavides', 2)
+INSERT INTO [dbo].[SUCURSAL] ([IDSUC], [NOMSUC], [DIRSUC], [IDJEFSUC]) VALUES (3, 'PizzaHut Nuevo Imperial', 'Av. La Mar', 3)
+INSERT INTO [dbo].[SUCURSAL] ([IDSUC], [NOMSUC], [DIRSUC], [IDJEFSUC]) VALUES (4, 'PizzaHut Surco', 'Jr. Ohiggins', 4)
+INSERT INTO [dbo].[SUCURSAL] ([IDSUC], [NOMSUC], [DIRSUC], [IDJEFSUC]) VALUES (5, 'PizzaHut Barranco', 'Jr. Manchego Muñoz', 5)
+INSERT INTO [dbo].[SUCURSAL] ([IDSUC], [NOMSUC], [DIRSUC], [IDJEFSUC]) VALUES (6, 'PizzaHut Miraflores', 'Av. Libertadores', 6)
+INSERT INTO [dbo].[SUCURSAL] ([IDSUC], [NOMSUC], [DIRSUC], [IDJEFSUC]) VALUES (7, 'PizzaHut Cerro Azul', 'Fundo Los Reyes', 7)
+INSERT INTO [dbo].[SUCURSAL] ([IDSUC], [NOMSUC], [DIRSUC], [IDJEFSUC]) VALUES (8, 'PizzaHut Quilmana', 'Av. Ayacucho', 8)
+INSERT INTO [dbo].[SUCURSAL] ([IDSUC], [NOMSUC], [DIRSUC], [IDJEFSUC]) VALUES (9, 'PizzaHut Asia', 'Av. Miguel Grau', 9)
+INSERT INTO [dbo].[SUCURSAL] ([IDSUC], [NOMSUC], [DIRSUC], [IDJEFSUC]) VALUES (10, 'PizzaHut Chilca', 'Fundo Santa Adela', 10)
+SET IDENTITY_INSERT [dbo].[SUCURSAL] OFF
